3626c34ca4d84aaf4606cba247aadff26d5019ff
Luis-Flores <lf016158@gmail.com>
2022-11-10 16:04:52 -0600

    se arreglo las demas pantallas

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# PagWebFinanciera
+# Proyecto Cartera Personal de Clientes
+
+Este proyecto se encarga de la página informativa de la empresa Mi Pyme Fiananciero
+
+
+## Construido con 🛠️
+
+* [Pho](https://www.php.net/) 
+* [Html](https://developer.mozilla.org/es/docs/Web/HTML)
+* [Css](https://developer.mozilla.org/es/docs/Web/CSS)
+* [JavaScripts](https://developer.mozilla.org/es/docs/Web/JavaScript)
+* [Ajax](https://api.jquery.com/jquery.ajax/) 
+
+## Versionado 📌
+
+Se utilizó [GitHub](https://github.com/) para el versionado. Para todas las versiones disponibles,
+mira [Pagina-Informativa-Mi-Pyme](https://github.com/IT-Mi-Pyme-Financiero/Pagina-Informativa-Mi-Pyme).
+
+## Autor ✒️
+
+* **Gaby Diaz**  - [gaby15diaz](https://github.com/gaby15diaz)
+* **Alex Salgado**  - [alexsalgado1197](https://github.com/alexsalgado1197)
+* **Luis Flores**  - [LuisFlores-C](https://github.com/Luis-FloresC)
+
+
+
+## Expresiones de Gratitud 🎁
+
+* Gracias a la Empresa de Mi Pyme Financiero 📢
+
+
+---
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,17 +10,19 @@ if(empty($_POST['name'])      ||
    return false;
    }
    
+   
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'info@mipymefinanciero.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulario de contacto del sitio web Mi Pyme Financiero";
+$email_body = "Ha recibido un nuevo mensaje del formulario de contacto de su sitio web Mi Pyme Financiero.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nCorreo Electrónico: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
+$headers = "From: info@mipymefinanciero.net\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+
+return mail($to,$email_subject,$email_body,$headers);
+         
 ?>
